6351fab58f40895a47f634b282df180399c808e0
nabil <thenabilarta@gmail.com>
2023-11-28 01:21:04 +0700

    Update

--- entity/answer.entity.go
+++ entity/answer.entity.go
@@ -14,6 +14,7 @@ type Answer struct {
 	UserId       uuid.UUID  `json:"-" gorm:"column:userId"`
 	User         *User      `json:"user" gorm:"foreignKey:userId"`
 	QuestionId   uuid.UUID  `json:"questionId" gorm:"column:questionId"`
+	Question     *Question  `json:"question" gorm:"foreignKey:questionId"`
 	Comment      *[]Comment `json:"comment" gorm:"foreignKey:answerId"`
 	IsShow       bool       `json:"-" gorm:"column:isShow;default:true"`
 	BaseEntity
