23f4cbf76477b25c696e329cf4efef0e7f45f8eb
nabil <thenabilarta@gmail.com>
2023-12-02 14:59:23 +0700

    Simple consultant data

--- entity/answer.entity.go
+++ entity/answer.entity.go
@@ -6,17 +6,17 @@ import (
 )
 
 type Answer struct {
-	Id           uuid.UUID  `json:"id" gorm:"column:id"`
-	Content      string     `json:"content" gorm:"column:content"`
-	Likes        int64      `json:"likes" gorm:"column:likes"`
-	TotalComment int64      `json:"totalComments" gorm:"column:totalComments"`
-	Tag          string     `json:"tag" gorm:"column:tag"`
-	UserId       uuid.UUID  `json:"-" gorm:"column:userId"`
-	User         *User      `json:"user" gorm:"foreignKey:userId"`
-	QuestionId   uuid.UUID  `json:"questionId" gorm:"column:questionId"`
-	Question     *Question  `json:"question" gorm:"foreignKey:questionId"`
-	Comment      *[]Comment `json:"comment" gorm:"foreignKey:answerId"`
-	IsShow       bool       `json:"-" gorm:"column:isShow;default:true"`
+	Id           uuid.UUID              `json:"id" gorm:"column:id"`
+	Content      string                 `json:"content" gorm:"column:content"`
+	Likes        int64                  `json:"likes" gorm:"column:likes"`
+	TotalComment int64                  `json:"totalComments" gorm:"column:totalComments"`
+	Tag          string                 `json:"tag" gorm:"column:tag"`
+	UserId       uuid.UUID              `json:"-" gorm:"column:userId"`
+	User         *UserConsultantProfile `json:"user" gorm:"foreignKey:userId"`
+	QuestionId   uuid.UUID              `json:"questionId" gorm:"column:questionId"`
+	Question     *Question              `json:"question" gorm:"foreignKey:questionId"`
+	Comment      *[]Comment             `json:"comment" gorm:"foreignKey:answerId"`
+	IsShow       bool                   `json:"-" gorm:"column:isShow;default:true"`
 	BaseEntity
 }
 
